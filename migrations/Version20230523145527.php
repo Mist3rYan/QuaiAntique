d95e277d6c64587c5c89ef70db59fbb5f06d6c06
Yannick <lanfyp@gmail.com>
2023-05-23 16:57:16 +0200

    Modif bundle

--- migrations/Version20230523145527.php
+++ migrations/Version20230523145527.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230523081511 extends AbstractMigration
+final class Version20230523145527 extends AbstractMigration
 {
     public function getDescription(): string
     {
